781b988335802d77a79021d9c74212b71e399ff3
rmendo55 <mendozarafael914@gmail.com>
2019-03-02 18:49:53 -0800

    Added Factorial code

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+
+    public static int factorial(int value) {
+        //base case
+        if (value == 1) {
+            return 1;
+        }
+        else {
+            return value * factorial(value - 1);
+        }
+    }
+
+
+
+}
